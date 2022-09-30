641c9cfd65dfddf24af7e123d66a2cfca4288daa
Kenan Bolat <kenan.bolat@yandex.com.tr>
2022-09-30 09:45:45 +0300

    Refactor the script

--- scrape.py
+++ scrape.py
@@ -13,13 +13,21 @@ DRIVER_PATH = './chromedriver'
 TINT_COLOR = (0, 0, 0)  # Black
 TRANSPARENCY = .35  # Degree of transparency, 0-100%
 OPACITY = int(255 * TRANSPARENCY)
+XPATH = '//*[@id="modifiedDate"]'
+FONT = "./fonts/roboto-mono-v22-latin-regular.ttf"
+FORMAT = "%Y%m%d-%H%M"
+SS_PATH = "./images"
 
 
 class Scrape(object):
 
     def __init__(self):
-        self.mobile_ss_name = f'{os.path.join("images", datetime.now().strftime("%Y%m%d-%H%M"))}_mobile.png'
-        self.desktop_ss_name = f'{os.path.join("images", datetime.now().strftime("%Y%m%d-%H%M"))}_desktop.png'
+        self.process_date = datetime.now().strftime(FORMAT)
+        self.ss_prefix = os.path.join(SS_PATH, self.process_date)
+        # Mobile Screen Shot Path
+        self.m_ss_path = f"{self.ss_prefix}_mobile.png"
+        # Desktop Screen Shot Path
+        self.dd_ss_path = f"{self.ss_prefix}_desktop.png"
         self.modified_date = None
 
     def get_desktop(self):
@@ -29,46 +37,56 @@ class Scrape(object):
         driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
         driver.get(URL)
 
-        self.modified_date = driver.find_element(By.XPATH, '//*[@id="modifiedDate"]').text.strip()
-        driver.save_screenshot(f'{self.desktop_ss_name}')
-        self.watermark(f'{self.desktop_ss_name}', "DESKTOP")
+        self.modified_date = driver.find_element(By.XPATH, XPATH).text.strip()
+        driver.save_screenshot(f'{self.dd_ss_path}')
+        self.watermark(f'{self.dd_ss_path}', "DESKTOP")
         driver.quit()
 
     def get_mobile(self):
         options = Options()
         options.headless = True
 
-        mobile_emulation = {"deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-                            "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) "
-                                         "AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 "
+        mobile_emulation = {"deviceMetrics": {"width": 360,
+                                              "height": 640,
+                                              "pixelRatio": 3.0},
+                            "userAgent": "Mozilla/5.0 "
+                                         "(Linux; Android 4.2.1; en-us; "
+                                         "Nexus 5 Build/JOP40D) "
+                                         "AppleWebKit/535.19 "
+                                         "(KHTML, like Gecko) "
+                                         "Chrome/18.0.1025.166 "
                                          "Mobile Safari/535.19"}
+
         options.add_experimental_option("mobileEmulation", mobile_emulation)
 
         driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
         driver.get(URL)
-        driver.save_screenshot(f'{self.mobile_ss_name}')
-        self.watermark(f'{self.mobile_ss_name}', "MOBILE")
+        driver.save_screenshot(f'{self.m_ss_path}')
+        self.watermark(f'{self.m_ss_path}', "MOBILE")
         driver.quit()
 
-    @staticmethod
-    def watermark(img_file_name, test_mode):
+    def watermark(self, img_file_name, test_mode):
+        """Watermark the screenshots for the visual comparison purposes"""
         img = Image.open(img_file_name)
-        draw = ImageDraw.Draw(img)
 
-        font = ImageFont.truetype("./fonts/roboto-mono-v22-latin-regular.ttf", 50)
-        text = f'GOC.STORE {test_mode} TEST {datetime.now().strftime("%Y%m%d-%H%M")}'
+        font = ImageFont.truetype(FONT, 50)
+
+        text = f'GOC.STORE {test_mode} ' \
+               f'TEST {self.process_date}'
+
         x, y = 5, 900
         w, h = font.getsize(text)
 
         overlay = Image.new('RGBA', img.size, TINT_COLOR + (0,))
-        draw = ImageDraw.Draw(overlay)  # Create a context for drawing things on it.
+        # Create a context for drawing things on it
+        draw = ImageDraw.Draw(overlay)
         draw.rectangle((x, y, x + w, y + h), fill=TINT_COLOR + (OPACITY,))
         draw.text((x, y), text, (255, 255, 0), font=font)
 
         overlay = overlay.rotate(45)
         img = Image.alpha_composite(img, overlay)
+        # Remove alpha for saving in jpg format.
         img = img.convert("RGB")  # Remove alpha for saving in jpg format.
-        # draw.text((x, y),"Sample Text",(r,g,b))
         img.save(os.path.join(img_file_name))
 
 
@@ -78,5 +96,5 @@ if __name__ == '__main__':
     scraper.get_mobile()
     modified_date = scraper.modified_date
     print(modified_date)
-    print(scraper.desktop_ss_name)
-    print(scraper.mobile_ss_name)
+    print(scraper.dd_ss_path)
+    print(scraper.m_ss_path)
